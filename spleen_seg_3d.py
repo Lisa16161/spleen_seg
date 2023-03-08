ba390f902fbb441e7bfd84b61c79cb8f036aed62
lisa16161 <zcemlat@ucl.ac.uk>
2023-03-08 12:25:37 +0000

    split in test/train/val Train_transforms -> RandAffine augmentation

--- spleen_seg_3d.py
+++ spleen_seg_3d.py
@@ -11,6 +11,8 @@ from monai.transforms import (
     SaveImaged,
     ScaleIntensityRanged,
     Spacingd,
+    RandAffined,
+
 )
 from torch.utils.tensorboard import SummaryWriter
 from monai.networks.nets import UNet
@@ -57,7 +59,7 @@ print(os.path.join(data_dir, "imagesTr", "*.nii.gz"))
 
 train_labels = sorted(glob.glob(os.path.join(data_dir, "labelsTr", "*.nii.gz")))
 data_dicts = [{"image": image_name, "label": label_name} for image_name, label_name in zip(train_images, train_labels)]
-train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+train_files, test_files, val_files = data_dicts[:29], data_dicts[29:34], data_dicts[34:]
 
 
 set_determinism(seed=0)
@@ -87,13 +89,13 @@ train_transforms = Compose(
             image_key="image",
             image_threshold=0,
         ),
-        # user can also add other random transforms
-        # RandAffined(
-        #     keys=['image', 'label'],
-        #     mode=('bilinear', 'nearest'),
-        #     prob=1.0, spatial_size=(96, 96, 96),
-        #     rotate_range=(0, 0, np.pi/15),
-        #     scale_range=(0.1, 0.1, 0.1)),
+         RandAffined(
+             keys=['image', 'label'],
+             mode=('bilinear', 'nearest'),
+             prob=1.0, spatial_size=(96, 96, 96),
+             rotate_range=(0, 0, np.pi/15),
+             scale_range=(0.1, 0.1, 0.1),
+         ),
     ]
 )
 val_transforms = Compose(
